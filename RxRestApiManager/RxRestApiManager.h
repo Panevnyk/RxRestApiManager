79140f7a81327287738492858320340fa5311daf
panevnyk <sieter3008@gmail.com>
2018-04-08 15:28:19 +0300

    Initial Commit

---
+++ RxRestApiManager/RxRestApiManager.h
@@ -0,0 +1,19 @@
+//
+//  RxRestApiManager.h
+//  RxRestApiManager
+//
+//  Created by Panevnyk Vlad on 4/8/18.
+//  Copyright © 2018 Panevnyk Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxRestApiManager.
+FOUNDATION_EXPORT double RxRestApiManagerVersionNumber;
+
+//! Project version string for RxRestApiManager.
+FOUNDATION_EXPORT const unsigned char RxRestApiManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxRestApiManager/PublicHeader.h>
+
+
